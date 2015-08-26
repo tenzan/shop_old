e538d364c13b847ebecbbe65d4d63871036668c3
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-08-26 12:13:34 +0900

    Listing all items on index page

---
+++ spec/features/viewing_items_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can view items' do
+  scenario 'with the item details' do
+    item = FactoryGirl.create(:item, name: 'Shampoo', quantity: 3,
+                              unit_price: 100.50, url: 'http://google.com',
+                              note: 'Looking forward!')
+
+    visit '/'
+    click_link 'Shampoo'
+    expect(page.current_url).to eq item_url(item)
+  end
+end
